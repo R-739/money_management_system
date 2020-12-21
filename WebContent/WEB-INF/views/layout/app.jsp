878c23f6d756db55f4e96f1c1c3d0b800f5d6871
ryuta <br85hyd222ki@yahoo.co.jp>
2020-12-21 10:14:29 +0900

    Add User new

--- WebContent/WEB-INF/views/layout/app.jsp
+++ WebContent/WEB-INF/views/layout/app.jsp
@@ -13,6 +13,17 @@
             <div id="header">
                 <h1>お金管理システム</h1>
             </div>
+            <div id="header_menu">
+            <c:if test="${sessionScope.login_employee !=null}">
+            <a href="<c:url value='/moneys/index' />">帳簿一覧</a>&nbsp;
+            </c:if>
+            </div>
+            <c:if test="${sessionScope.login_employee != null}">
+                <div id="employee_name">
+                <c:out value="${sessionScope.login_employee.name}" />&nbsp;さん&nbsp;&nbsp;&nbsp;
+                <a href="<c:url value='/logout' />">ログアウト</a>
+                </div>
+            </c:if>
                 <div id="content">
                     <div class="text-center">
                         ${param.content}
--- src/controllers/users/UsersNewServlet.java
+++ src/controllers/users/UsersNewServlet.java
@@ -1,4 +1,4 @@
-package controllers.toppage;
+package controllers.users;
 
 import java.io.IOException;
 
@@ -9,17 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.User;
+
 /**
- * Servlet implementation class TopPageIndexServlet
+ * Servlet implementation class UsersNewServlet
  */
-@WebServlet("/index.html")
-public class TopPageIndexServlet extends HttpServlet {
+@WebServlet("/users/new")
+public class UsersNewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TopPageIndexServlet() {
+    public UsersNewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,12 +31,10 @@ public class TopPageIndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        if(request.getSession().getAttribute("flush") != null) {
-            request.setAttribute("flush", request.getSession().getAttribute("flush"));
-            request.getSession().removeAttribute("flush");
-        }
-        
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("user", new User());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/new.jsp");
         rd.forward(request, response);
     }
 
